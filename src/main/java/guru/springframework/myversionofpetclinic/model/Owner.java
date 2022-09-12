62ad91ac8af8a16934e1babba9016ea6a9ba8eea
Dragos Lazea <dragos.lazea@student.utcluj.ro>
2022-09-12 11:57:02 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/myversionofpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.myversionofpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/myversionofpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.myversionofpetclinic.model;
+
+public class Vet extends Person {
+}
