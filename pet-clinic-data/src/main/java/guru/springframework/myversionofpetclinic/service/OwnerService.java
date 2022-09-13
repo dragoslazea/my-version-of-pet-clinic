a8e2e8f905c23f9cc2a0d7e610d134c8954eae61
Dragos Lazea <dragos.lazea@student.utcluj.ro>
2022-09-13 17:45:10 +0300

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/springframework/myversionofpetclinic/service/OwnerService.java
@@ -0,0 +1,16 @@
+package guru.springframework.myversionofpetclinic.service;
+
+import guru.springframework.myversionofpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
